99692dd323b400d01ec31638bca55c48e9749dd8
J.Jusman <juswaldy@gmail.com>
2011-09-19 02:04:55 -0700

    Change team views

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,10 +2,9 @@ class Team < ActiveRecord::Base
   has_and_belongs_to_many :players
   validates :name, :presence => true, :uniqueness => true
   validates :score, :numericality => {:greater_than_or_equal_to => 0}
-  attr_accessor :strength
-  after_initialize :init
 
-  def init
-    @strength = 0
+  # Sum up all the strengths of each player.
+  def strength
+    players.map{|p| p.strength}.reduce{|sum,n| sum + n}
   end
 end
