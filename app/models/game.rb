a72901046b6152212dd50f552ec7a9b3939f6ecd
J.Jusman <juswaldy@gmail.com>
2011-09-19 00:58:11 -0700

    Add Game validation and fix new/edit views

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,9 @@
 class Game < ActiveRecord::Base
-  validates :team1id, :presence => true
-  validates :team2id, :presence => true
-  validates :score, :numericality => {:greater_than_or_equal_to => 0}
-  validates :assist, :numericality => {:greater_than_or_equal_to => 0}
+  validates :team1score, :numericality => {:greater_than_or_equal_to => 0}
+  validates :team1score, :numericality => {:greater_than_or_equal_to => 0}
+  validate :different_teams
+
+  def different_teams
+    errors.add('Error:', 'Can''t have a team playing against itself') unless self.team1id != self.team2id
+  end
 end
