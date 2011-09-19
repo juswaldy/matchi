7114ccd0e4472537c0072b5859f4c99a1758e5dc
J.Jusman <juswaldy@gmail.com>
2011-09-19 01:34:34 -0700

    Change welcome view, validate team name uniqueness

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,6 @@
 class Team < ActiveRecord::Base
   has_and_belongs_to_many :players
+  validates :name, :presence => true, :uniqueness => true
   validates :score, :numericality => {:greater_than_or_equal_to => 0}
   attr_accessor :strength
   after_initialize :init
