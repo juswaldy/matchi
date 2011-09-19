dfb7bcb699708845978a62f7176196cea2797b3c
J.Jusman <juswaldy@gmail.com>
2011-09-18 22:36:23 -0700

    Removed id from players_teams and remigrate, also added validations and attrs for Player and Team

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,12 @@
 class Player < ActiveRecord::Base
+  has_and_belongs_to_many :teams
+  validates :firstname, :presence => true
+  validates :lastname, :presence => true
+  validates :score, :numericality => {:greater_than_or_equal_to => 0}
+  validates :assist, :numericality => {:greater_than_or_equal_to => 0}
+
+  def strength
+    experience = 1
+    @score * 5 + @assist * 3 + experience
+  end
 end
--- db/migrate/20110919040939_create_players_teams.rb
+++ db/migrate/20110919040939_create_players_teams.rb
@@ -1,6 +1,6 @@
 class CreatePlayersTeams < ActiveRecord::Migration
   def change
-    create_table :players_teams do |t|
+    create_table :players_teams, :id => false do |t|
       t.integer :player_id, :null => false
       t.integer :team_id, :null => false
 
