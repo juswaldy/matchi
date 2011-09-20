b2360a28e3aa5876f8ba3c9be2efa31abaec9f59
J.Jusman <juswaldy@gmail.com>
2011-09-19 23:43:49 -0700

    Combine both algorithms to give better results, also fix bug in second algorithm

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,27 +8,45 @@ class WelcomeController < ApplicationController
     team.potential += player.strength
   end
 
-  # Get info for the list of available players,
-  # then sort the list based on strength, and then
-  # break up the list into 3 player slices and assign
-  # to each team.
-  # Then pick the weakest player and assign it to
-  # balance the number of players in teams.
-  def suggestion
-    @present = Player.find_all_by_id(params[:player_ids])
-    playersByStrength = @present.sort_by{|x| [-x.strength, x.nickname]}
-    @team1 = Team.new
-    @team2 = Team.new
+  def algo1(players) 
+    # Sort the list based on strength, and then break up
+    # the list into 2 player slices and assign alternately.
+    playersByStrength = players.sort_by{|x| [-x.strength, x.nickname]}
+    team1 = Team.new
+    team2 = Team.new
+    playersByStrength.each_slice(2) do |p1, p2|
+      # Assign the strongest player in this slice to
+      # the weaker team, and the next strongest to the
+      # other team.
+      if team1.potential > team2.potential
+        assign(team2, p1)
+        assign(team1, p2) if p2
+      else
+        assign(team1, p1)
+	assign(team2, p2) if p2
+      end
+    end
+    [team1, team2]
+  end
+
+  def algo2(players)
+    # Sort the list based on strength, and then break up
+    # the list into 3 player slices and assign to teams.
+    # Then pick the weakest player and assign it to
+    # balance the number of players in teams.
+    playersByStrength = players.sort_by{|x| [-x.strength, x.nickname]}
+    team1 = Team.new
+    team2 = Team.new
     playersByStrength.each_slice(3) do |p1, p2, p3|
       # Assign the strongest player in this slice to
       # the weaker team, and the next strongest to the
       # other team.
-      if @team1.potential > @team2.potential
-        assign(@team2, p1)
-        assign(@team1, p2) if p2
+      if team1.potential > team2.potential
+        assign(team2, p1)
+        assign(team1, p2) if p2
       else
-        assign(@team1, p1)
-	assign(@team2, p2) if p2
+        assign(team1, p1)
+	assign(team2, p2) if p2
       end
 
       # The third player p3 is assigned to the weaker team
@@ -36,14 +54,39 @@ class WelcomeController < ApplicationController
       # is assigned to the other team to balance the number
       # of players.
       p4 = playersByStrength.pop
-      if @team1.potential > @team2.potential
-        assign(@team2, p3) if p3
-	assign(@team1, p4) if p4
+      if team1.potential > team2.potential
+        assign(team2, p3) if p3
+	assign(team1, p4) if p4 and p4 != p3
       else
-        assign(@team1, p3) if p3
-	assign(@team2, p4) if p4
+        assign(team1, p3) if p3
+	assign(team2, p4) if p4 and p4 != p3
       end
     end
+    [team1, team2]
+  end
+
+  # Get info for the list of available players,
+  # then run different algorithms to get best match.
+  def suggestion
+    @players = Player.find_all_by_id(params[:player_ids])
+    suggestions = {}
+    approaches = []
+    approaches.push(:algo1) # Add new algorithms here.
+    approaches.push(:algo2) # These are defined above.
+    approaches.each do |algorithm|
+      # For each algorithm, run it, passing the players.
+      # And then calculate the delta in strengths.
+      # Keep these suggestions in a hash.
+      team1, team2 = send algorithm, @players
+      delta = (team1.strength - team2.strength).abs
+      suggestions[delta] = [team1, team2]
+    end
+
+    # Get the best suggestion. (smallest delta, found by sorting)
+    # TODO: present all the suggestions to the user, instead of just 1.
+    # TODO: also generate different combinations for each algorithm.
+    best = suggestions.keys.sort[0]
+    @team1, @team2 = suggestions[best]
     @max = [@team1.players.to_a.count, @team2.players.to_a.count].max - 1
   end
 
