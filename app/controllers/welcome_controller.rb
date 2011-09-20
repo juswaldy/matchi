7dd832f6fa67a28731288339e1264c4e576f6db6
J.Jusman <juswaldy@gmail.com>
2011-09-19 20:50:58 -0700

    Change algorithm to assign 3 strongest + 1 weakest players at a time

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,19 +10,38 @@ class WelcomeController < ApplicationController
 
   # Get info for the list of available players,
   # then sort the list based on strength, and then
-  # break up the list into 2 player slices and assign
+  # break up the list into 3 player slices and assign
   # to each team.
+  # Then pick the weakest player and assign it to
+  # balance the number of players in teams.
   def suggestion
     @present = Player.find_all_by_id(params[:player_ids])
+    playersByStrength = @present.sort_by{|x| [-x.strength, x.nickname]}
     @team1 = Team.new
     @team2 = Team.new
-    @present.sort_by{|x| [-x.strength, x.nickname]}.each_slice(2) do |player, nextplayer|
+    playersByStrength.each_slice(3) do |p1, p2, p3|
+      # Assign the strongest player in this slice to
+      # the weaker team, and the next strongest to the
+      # other team.
       if @team1.potential > @team2.potential
-        assign(@team2, player)
-        assign(@team1, nextplayer) if nextplayer
+        assign(@team2, p1)
+        assign(@team1, p2) if p2
       else
-        assign(@team1, player)
-	assign(@team2, nextplayer) if nextplayer
+        assign(@team1, p1)
+	assign(@team2, p2) if p2
+      end
+
+      # The third player p3 is assigned to the weaker team
+      # and the fourth player p4 is the weakest player that
+      # is assigned to the other team to balance the number
+      # of players.
+      p4 = playersByStrength.pop
+      if @team1.potential > @team2.potential
+        assign(@team2, p3) if p3
+	assign(@team1, p4) if p4
+      else
+        assign(@team1, p3) if p3
+	assign(@team2, p4) if p4
       end
     end
     @max = [@team1.players.to_a.count, @team2.players.to_a.count].max - 1
